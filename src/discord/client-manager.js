0d69db27d378fe9f9a3995595af13e18391cd670
Kritika.vig <kritikavig2003@gmail.com>
2023-09-17 22:46:32 +0530

    files added

---
+++ src/discord/client-manager.js
@@ -0,0 +1,115 @@
+const audioPlayer = require('../audio');
+const logger = require('../utils/logger');
+
+// import { Client, GatewayIntentBits } from "discord.js";
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+      intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildVoiceStates,
+      ],
+    });
+    
+const getVoiceChannelId = (interaction) => {
+  const guild = client.guilds.cache.get(interaction.guildId);
+  const member = guild.members.cache.get(interaction.member.user.id);
+  return member.voice.channelId;
+};
+
+client.on('ready', async () => {
+  await logger.initializeLogger(client);
+});
+
+client.on('interactionCreate', async interaction => {
+  if(!interaction.isCommand()) return;
+
+  // get guild information
+  const guild = client.guilds.cache.get(interaction.guildId);
+
+  // get voice channel id user is connected to
+  const voiceChannelId = getVoiceChannelId(interaction);
+
+  // if voiceChannelId is null, user is not in a voice channel
+  if(!voiceChannelId){
+    await interaction.reply('You must be in a voice channel to play audio!');
+    return;
+  }
+  
+  const { commandName, options } = interaction;
+
+  logger.logInfo(`processing interaction { Command: ${commandName}, params: ${options.getString('query')}, user: ${interaction.user.username}}`);
+
+  // determine which action to take based on command
+  switch(commandName) {
+    case 'play':
+      try{
+        const searchQuery = options.getString('query');
+
+        await audioPlayer.playSong(client, guild, interaction.channelId, voiceChannelId, searchQuery);
+        await interaction.reply(`Processing ${commandName} command...`);
+      }catch(err){
+        logger.logError('error on play');
+        logger.logError(err);
+
+        if(interaction.isRepliable()){
+          await interaction.reply(`Error playing ${options.getString('query')}: ${err}`)
+        }
+      }
+      break;
+    
+    case 'stop':
+      try{
+        await audioPlayer.stopPlayer(guild, interaction);
+      }catch(err){
+        logger.logError('error on stop');
+        logger.logError(err);
+      }
+      break;
+
+    case 'skip':
+      try{
+        await interaction.reply('Skipping Song...')
+        await audioPlayer.skip(client, guild, interaction.channelId, voiceChannelId);
+      }catch(err){
+        logger.logError('error on skip');
+        logger.logError(err);
+      }
+      break;
+    
+    case 'pause':
+      try{
+        const returnMessage = await audioPlayer.pause(guild.id);
+        await interaction.reply(returnMessage);
+      }catch(err){
+        logger.logError('error on pause');
+        logger.logError(err);
+      }
+      break;
+    
+    case 'queue':
+      try{
+        const queue = audioPlayer.getQueue(guild.id);
+        await interaction.reply(queue);
+      }catch(err){
+        logger.logError('error on queue');
+        logger.logError(err);
+      }
+      break;
+
+    default:
+      await interaction.reply('Invalid command');
+  }
+});
+
+client.on('error', (error) => {
+  logger.logError(error);
+});
+
+/**
+ * initialize client
+ * @param {*} token discord api token
+ */
+exports.initializeClient = (token) => {
+  client.login(token);
+};
